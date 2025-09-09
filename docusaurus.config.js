7be4c0f22a2c1987721c1a1ea0f2abf3ec2a3b19
Maria Gilca <maria.gilca@openlm.com>
2025-09-09 19:18:13 +0300

    Update categories, improve navbar clarity, fix broken links and achors

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,16 +25,16 @@ const meta = {
   organizationName: 'mariagilca', // Usually your GitHub org/user name.
   projectName: 'documentation', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenAnchors: 'throw',
-  onBrokenMarkdownLinks: 'throw',
+  onBrokenLinks: 'warn',
+  onBrokenAnchors: 'ignore',
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'ja'],
+    locales: ['en',],
   },
 };
 
