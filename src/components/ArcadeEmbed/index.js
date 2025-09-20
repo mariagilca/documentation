16ea75477a604a47b51c504dbcf489e039c683b9
Maria Gilca <maria.gilca@openlm.com>
2025-09-20 20:32:36 +0300

    Fix font on the Arcade interactive button, which was not similar to the rest of the site, update the Arcade embedd code to a new one that fixes chapter issues

--- src/components/ArcadeEmbed/index.js
+++ src/components/ArcadeEmbed/index.js
@@ -2,7 +2,7 @@ export function ArcadeEmbed() {
   return (
     <div style={{ position: 'relative', paddingBottom: 'calc(52.7756059421423% + 41px)', height: 0, width: '100%' }}>
       <iframe
-        src="https://demo.arcade.software/X1ZcqokdTaD0aFKAlFzL?embed&embed_mobile=inline&embed_desktop=inline&show_copy_link=true"
+        src="https://demo.arcade.software/dIJf3kJCT0mFSmhg3IcM?embed&embed_mobile=tab&embed_desktop=inline&show_copy_link=true"
         title="Getting started with OpenLM"
         frameBorder="0"
         loading="lazy"
