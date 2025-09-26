f0e125e433918281aec31f1d55d3d969fa4394f6
Maria Gilca <maria.gilca@openlm.com>
2025-09-26 14:51:29 +0000

    Merged PR 26702: Upgrade docusaurus
    Upgrade docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,8 +35,16 @@ const meta = {
 
   onBrokenLinks: 'warn',
   onBrokenAnchors: 'ignore',
-  onBrokenMarkdownLinks: 'warn',
 
+  // mermaid support
+  themes: ['@docusaurus/theme-mermaid'],
+
+  markdown: {
+    mermaid: true,
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+  },
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
@@ -120,14 +128,6 @@ const plugins = [
 const config = {
   ...meta,
   plugins,
-
-  // mermaid support
-  themes: ['@docusaurus/theme-mermaid'],
-  markdown: {
-    mermaid: true,
-  },
-  //
-  
   presets: [
     [
       'classic',
